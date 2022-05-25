51e78d2d8eed6040de8b490e4a75b11e8cb5c423
haedoang <haedoang@gmail.com>
2022-05-25 15:20:36 +0900

    first commit

---
+++ ReadMe.md
@@ -0,0 +1,8 @@
+
+
+
+Run
+> docker run -d -p80:8080 -e SPRING_PROFILES_ACTIVE=blue haedoang/testwebapp 
+
+Build
+> docker build -t haedoang/testwebapp .
---
+++ src/main/java/io/haedoang/infra/Application.java
@@ -0,0 +1,30 @@
+package io.haedoang.infra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+@RestController
+public class Application {
+    private Environment env;
+
+    public Application(Environment env) {
+        this.env = env;
+    }
+
+    @GetMapping("api/v1/profiles")
+    public String getProfiles() {
+        return Arrays.toString(env.getActiveProfiles());
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
